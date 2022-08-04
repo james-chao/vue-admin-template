6af5690aee068f4b20337db5a4a0621ec1580cb6
James Chao (趙福春) <James.Chao@quantatw.com>
2022-08-04 15:22:13 +0800

    app.js Add "user-name">Welcome

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -2,8 +2,7 @@ import Cookies from 'js-cookie'
 
 const state = {
   sidebar: {
-    // opened: Cookies.get('sidebarStatus') ? !!+Cookies.get('sidebarStatus') : true,
-    opened: true,
+    opened: Cookies.get('sidebarStatus') ? !!+Cookies.get('sidebarStatus') : true,
     withoutAnimation: false
   },
   device: 'desktop'
@@ -11,7 +10,7 @@ const state = {
 
 const mutations = {
   TOGGLE_SIDEBAR: state => {
-    // state.sidebar.opened = !state.sidebar.opened
+    state.sidebar.opened = !state.sidebar.opened
     state.sidebar.withoutAnimation = false
     if (state.sidebar.opened) {
       Cookies.set('sidebarStatus', 1)
@@ -21,7 +20,7 @@ const mutations = {
   },
   CLOSE_SIDEBAR: (state, withoutAnimation) => {
     Cookies.set('sidebarStatus', 0)
-    // state.sidebar.opened = false
+    state.sidebar.opened = false
     state.sidebar.withoutAnimation = withoutAnimation
   },
   TOGGLE_DEVICE: (state, device) => {
